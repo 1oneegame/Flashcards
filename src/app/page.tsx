f943d8161bdd77d8f4bdd6e14fb6f7c6313380e3
1oneegame <95096078+1oneegame@users.noreply.github.com>
2025-04-24 20:58:50 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,77 @@
-import Image from "next/image";
+import { Progress } from "@/components/ui/progress";
+import { ArrowLeft, ArrowRight, Bookmark, CopyPlus, Ellipsis, FileText, GraduationCap, IdCard, Maximize, Play, Share, Shuffle, Star } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const Title = "Title sample", Rating = "4.0 (2 reviews)";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const cardFunction = [
+    {
+      title: "Flashcards",
+      href: "/",
+      icon: <IdCard className="w-8 h-8 text-blue-600"/>,
+    },
+    {
+      title: "Learn",
+      href: "/",
+      icon: <GraduationCap className="w-8 h-8 text-blue-600"/>,
+    },
+    {
+      title: "Test",
+      href: "/",
+      icon: <FileText className="w-8 h-8 text-blue-600"/>,
+    },
+    {
+      title: "Match",
+      href: "/",
+      icon: <CopyPlus className="w-8 h-8 text-blue-600"/>,
+    },
+  ]
+
+  return (
+    <div className="flex flex-col min-h-screen w-full">
+      <div className="flex flex-row items-center justify-between h-[75px] w-full border-1 py-4">
+          <div className="flex flex-col items-center justify-center ml-32 gap-1">
+            <h2 className="text-4xl font-bold cursor-pointer">{Title}</h2>
+            <div className="flex flex-row gap-1">
+              <Star className="w-5 h-5 pt-1"/>
+              <span className="text-md">{Rating}</span>
+            </div>
+          </div>
+          <div className="flex flex-row gap-4 items-center mr-32">
+            <button className="bg-white hover:bg-indigo-100 duration-300 transition-colors rounded-lg shadow-md cursor-pointer"><Bookmark className="w-6 h-6 m-2"/></button>
+            <button className="bg-white hover:bg-indigo-100 duration-300 transition-colors rounded-lg shadow-md cursor-pointer"><Share className="w-6 h-6 m-2"/></button>
+            <button className="bg-white hover:bg-indigo-100 duration-300 transition-colors rounded-lg shadow-md cursor-pointer"><Ellipsis className="w-6 h-6 m-2" /></button>
+          </div>
+      </div>
+      <div className="grid grid-cols-4 gap-8 mx-42 mt-8">
+        {
+          cardFunction.map((item, index) => (
+            <div key={index} className="cursor-pointer border-1 shadow-md rounded-lg w-full min-h-[125px] flex flex-col items-center justify-center hover:scale-105 transition-transform duration-300 bg-indigo-100/20">
+              {item.icon}
+              <h2 className="text-xl">{item.title}</h2>
+            </div>
+          ))
+        }
+      </div>
+      <div className="mx-42 mt-8 flex flex-col border-1 rounded-lg shadow-lg h-[350px]">
+        card content
+      </div>
+      <div className="mx-42 mt-8 flex flex-col gap-8">
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row gap-8">
+            <Link href="/" className="border-1 rounded-full shadow-md p-2 bg-white hover:bg-indigo-100 transition-colors duration-300"><Play className="w-6 h-6"/></Link>
+            <Link href="/" className="border-1 rounded-full shadow-md p-2 bg-white hover:bg-indigo-100 transition-colors duration-300"><Shuffle className="w-6 h-6"/></Link>
+          </div>
+          <div className="flex flex-row gap-8">
+            <Link href="/" className="border-1 rounded-full shadow-md p-2 bg-white hover:bg-indigo-100 transition-colors duration-300"><ArrowLeft className="w-6 h-6"/></Link>
+            <span className="text-3xl font-light">2/18</span>
+            <Link href="/" className="border-1 rounded-full shadow-md p-2 bg-white hover:bg-indigo-100 transition-colors duration-300"><ArrowRight className="w-6 h-6"/></Link>
+          </div>
+          <Link href="/" className="border-1 rounded-full shadow-md p-2 bg-white hover:bg-indigo-100 transition-colors duration-300"><Maximize/></Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <Progress value={95}/>
+      </div>
     </div>
   );
 }
